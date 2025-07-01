f96d19ded02ba331d5defa5fe0ae85fe6b4ec082
K0 <109837094+TotallyNotK0@users.noreply.github.com>
2025-06-30 21:16:25 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,3 @@
 #!/bin/sh
-echo "Beginning Poly build process..."
-if ! [ -f /usr/bin/nuitka ]; then
-    echo "Nuitka was not found on your system. Continue anyways? (y/N)"
-    read -n1 nuitka_continue
-    if [ "$nuitka_continue" != "y" ]; then
-        exit
-    fi
-fi
-
 nuitka --onefile --standalone --remove-output --enable-plugin=tk-inter --windows-icon-from-ico=ICON.ico --output-dir=dist poly.py
-
 mv dist/poly.bin dist/poly
-
-echo "Build complete!"
