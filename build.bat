7e5428853a399c478254b1c15bc8e9fb6885f54d
Raven <contact@ravendevteam.org>
2025-09-13 17:54:13 -0500

    Update build script using new method

--- build.bat
+++ build.bat
@@ -1,3 +1,6 @@
 @echo off
-nuitka --onefile --standalone --remove-output --enable-plugin=tk-inter --windows-icon-from-ico=ICON.ico --output-dir=dist poly.py
+set FileVersion=1.0.0.0
+set ProductVersion=1.2.0.0
+
+python -m nuitka --onefile --standalone --enable-plugins=tk-inter --remove-output --output-dir=dist --output-filename=Poly.exe --follow-imports --product-name="Poly" --company-name="Raven Development Team" --file-description="A terminal multiplexer with tabs, autocomplete, and more." --file-version=%FileVersion% --product-version=%ProductVersion% --copyright="Copyright (c) 2025 Raven Development Team" --onefile-tempdir-spec="{CACHE_DIR}\RavenDevelopmentTeam\Poly\{VERSION}" poly.py
 pause
