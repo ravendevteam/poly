19ad6e5a4eebfb2db040c80ba3074bcdcc748290
Raven <contact@ravendevteam.org>
2025-09-13 17:55:23 -0500

    Add back arg for window icon in build script

--- build.bat
+++ build.bat
@@ -2,5 +2,5 @@
 set FileVersion=1.0.0.0
 set ProductVersion=1.2.0.0
 
-python -m nuitka --onefile --standalone --enable-plugins=tk-inter --remove-output --output-dir=dist --output-filename=Poly.exe --follow-imports --product-name="Poly" --company-name="Raven Development Team" --file-description="A terminal multiplexer with tabs, autocomplete, and more." --file-version=%FileVersion% --product-version=%ProductVersion% --copyright="Copyright (c) 2025 Raven Development Team" --onefile-tempdir-spec="{CACHE_DIR}\RavenDevelopmentTeam\Poly\{VERSION}" poly.py
+python -m nuitka --onefile --standalone --enable-plugins=tk-inter --remove-output --output-dir=dist --output-filename=Poly.exe --windows-icon-from-ico=ICON.ico --follow-imports --product-name="Poly" --company-name="Raven Development Team" --file-description="A terminal multiplexer with tabs, autocomplete, and more." --file-version=%FileVersion% --product-version=%ProductVersion% --copyright="Copyright (c) 2025 Raven Development Team" --onefile-tempdir-spec="{CACHE_DIR}\RavenDevelopmentTeam\Poly\{VERSION}" poly.py
 pause
